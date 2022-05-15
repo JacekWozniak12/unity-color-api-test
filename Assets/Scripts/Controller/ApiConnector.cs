86544703b7bf1d64f4f93a6342701ad136fc7443
Jacek Woźniak <jac3kw0zniak@gmail.com>
2022-05-15 02:06:46 +0200

    moving from coroutine to async task

--- Assets/Scripts/Controller/ApiConnector.cs
+++ Assets/Scripts/Controller/ApiConnector.cs
@@ -1,36 +1,32 @@
 using System;
 using System.Text;
-using System.Collections;
 using UnityEngine;
 using UnityEngine.Networking;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System.Collections.Generic;
 using UnityEngine.Events;
+using System.Threading.Tasks;
 
 public class ApiConnector : MonoBehaviour
 {
     const string API_ADDRESS = "http://colormind.io/api/";
-
     public List<Color> CurrentColors = new List<Color>(5);
     public static ApiConnector Instance;
 
-    void Awake()
-    {
-        Instance = this;
-    }
+    void Awake() => Instance = this;
 
     /// <summary>
     /// Tries to connect with server specified in API_ADDRESS
     /// </summary>
     public UnityEvent<Color[]> ColorReady = new UnityEvent<Color[]>();
 
-    public void RequestColorScheme()
+    public async void RequestColorScheme()
     {
-        StartCoroutine(SendRequestColorScheme());
+        await SendRequestColorScheme();
     }
 
-    public void RequestColorScheme(Dictionary<int, Color> dictionary)
+    public async void RequestColorScheme(Dictionary<int, Color> dictionary)
     {
         string part = "{\"input\":[";
 
@@ -46,11 +42,11 @@ public class ApiConnector : MonoBehaviour
         }
 
         part += "],\"model\":\"default\"}";
-        Debug.Log(part);
-        StartCoroutine(SendRequestColorScheme(part));
+
+        await SendRequestColorScheme(part);
     }
 
-    public void RequestColorScheme(Color color, int index)
+    public async void RequestColorScheme(Color color, int index)
     {
         string part = "{\"input\":[";
         byte[] byteColor = ColorRangeConverter.ColorToRGB255_Byte(color.r, color.g, color.b);
@@ -63,68 +59,71 @@ public class ApiConnector : MonoBehaviour
         }
 
         part += "],\"model\":\"default\"}";
-        StartCoroutine(SendRequestColorScheme(part));
+        await SendRequestColorScheme(part);
     }
 
-    IEnumerator SendRequestColorScheme(string data = "{\"model\" : \"default\"}")
+    UnityWebRequest SetColorSchemeRequest(string request, byte[] dataBytes)
     {
-        string request = API_ADDRESS;
+        UnityWebRequest colorSchemeInfoRequest = new UnityWebRequest(request, "GET");
+        colorSchemeInfoRequest.uploadHandler = new UploadHandlerRaw(dataBytes);
+        colorSchemeInfoRequest.downloadHandler = new DownloadHandlerBuffer();
+        colorSchemeInfoRequest.SetRequestHeader("Content-Type", "application/json");
+        colorSchemeInfoRequest.SetRequestHeader("Accept", " text/plain");
+        return colorSchemeInfoRequest;
+    }
+
+    async Task SendRequestColorScheme(string data = "{\"model\" : \"default\"}")
+    {
+        string requestAddress = API_ADDRESS;
         var dataBytes = Encoding.UTF8.GetBytes(data);
 
-        using (UnityWebRequest colorSchemeInfoRequest = new UnityWebRequest(request, "GET"))
+        UnityWebRequest colorSchemeInfoRequest = SetColorSchemeRequest(requestAddress, dataBytes);
+        UnityWebRequest.Result result = await colorSchemeInfoRequest.SendWebRequest();
+
+        if (
+            result == UnityWebRequest.Result.ConnectionError
+            ||
+            result == UnityWebRequest.Result.ProtocolError)
         {
-            colorSchemeInfoRequest.uploadHandler = new UploadHandlerRaw(dataBytes);
-            colorSchemeInfoRequest.downloadHandler = new DownloadHandlerBuffer();
-            colorSchemeInfoRequest.SetRequestHeader("Content-Type", "application/json");
-            colorSchemeInfoRequest.SetRequestHeader("Accept", " text/plain");
-            yield return colorSchemeInfoRequest.SendWebRequest();
-
-            if (
-                colorSchemeInfoRequest.result == UnityWebRequest.Result.ConnectionError
-                ||
-                colorSchemeInfoRequest.result == UnityWebRequest.Result.ProtocolError)
-            {
-                Debug.LogError(colorSchemeInfoRequest.error);
-                Popup.Instance.RequestErrorMessage(colorSchemeInfoRequest.error);
-            }
-            else
-            {
-                // Debug.Log(colorSchemeInfoRequest.downloadHandler.text);
-                
-                try
-                {
-                    JObject t = JsonConvert.DeserializeObject<JObject>(colorSchemeInfoRequest.downloadHandler.text);
-                    List<int>[] colors = t.GetValue("result").ToObject<List<int>[]>();
-                    List<Color> temp = new List<Color>();
-
-                    foreach (List<int> l in colors)
-                    {
-                        Color c = new Color(l[0], l[1], l[2]);
-
-                        string message = string.Format(
-                            "<color=#{0:X2}{1:X2}{2:X2}>{3}</color>",
-                            (byte)(c.r),
-                            (byte)(c.g),
-                            (byte)(c.b),
-                            c);
-
-                        // Debug.Log(message);
-
-                        temp.Add(c);
-                    }
-
-                    CurrentColors = temp;
-                    ColorReady?.Invoke(temp.ToArray());
-                }
-                catch (JsonException e)
-                {
-                    Popup.Instance.RequestErrorMessage(e.Message);
-                }
-            }
-
+            Popup.Instance.RequestErrorMessage(colorSchemeInfoRequest.error);
         }
+        else
+        {
+            try
+            {
+                JObject t = JsonConvert.DeserializeObject<JObject>(colorSchemeInfoRequest.downloadHandler.text);
+                List<int>[] colors = t.GetValue("result").ToObject<List<int>[]>();
+                List<Color> temp = CreateListOfColorsFromIntArray(colors);
+                CurrentColors = temp;
+                ColorReady?.Invoke(temp.ToArray());
+            }
+            catch (JsonException e)
+            {
+                Popup.Instance.RequestErrorMessage(e.Message);
+            }
+        }
+
+    }
+
+    private List<Color> CreateListOfColorsFromIntArray(List<int>[] colors)
+    {
+        List<Color> temp = new List<Color>();
+
+        foreach (List<int> l in colors)
+        {
+            Color c = new Color(l[0], l[1], l[2]);
+
+            string message = string.Format(
+                "<color=#{0:X2}{1:X2}{2:X2}>{3}</color>",
+                (byte)(c.r), (byte)(c.g), (byte)(c.b), c);
+
+            temp.Add(c);
+        }
+
+        return temp;
     }
 }
+
 
 
 
---
+++ Assets/Scripts/Controller/UnityWebRequestExtension.cs
@@ -0,0 +1,14 @@
+using UnityEngine.Networking;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+public static class UnityWebRequestExtension
+{
+    public static TaskAwaiter<UnityWebRequest.Result> GetAwaiter(this UnityWebRequestAsyncOperation requestOperation)
+    {
+        TaskCompletionSource<UnityWebRequest.Result> tsc = new TaskCompletionSource<UnityWebRequest.Result>();
+        requestOperation.completed += asyncOperation => tsc.TrySetResult(requestOperation.webRequest.result);
+        if (requestOperation.isDone) tsc.TrySetResult(requestOperation.webRequest.result);
+        return tsc.Task.GetAwaiter();
+    }
+}
